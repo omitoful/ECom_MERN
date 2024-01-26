f2b775ab96ac032b91618296b2a65d22dd30a357
Kevin <kevin@twjoin.com>
2024-01-26 10:49:18 +0800

    setup nav bar & add font: poppins

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 
 const Navbar = () => {
+
+    const [menu, setMenu] = useState("shop");
+
   return (
     <div className='navbar'>
         <div className='nav-logo'>
@@ -11,14 +14,15 @@ const Navbar = () => {
             <p>SHOPPER</p>
         </div>
         <ul className="nav-menu">
-            <li>SHOP</li>
-            <li>MEN</li>
-            <li>WOMEN</li>
-            <li>KIDS</li>
+            <li onClick={() => setMenu("shop")}>SHOP {menu === "shop" ? <hr/> : <></>}</li>
+            <li onClick={() => setMenu("men")}>MEN {menu === "men" ? <hr/> : <></>}</li>
+            <li onClick={() => setMenu("women")}>WOMEN {menu === "women" ? <hr/> : <></>}</li>
+            <li onClick={() => setMenu("kids")}>KIDS {menu === "kids" ? <hr/> : <></>}</li>
         </ul>
         <div className="nav-login-cart">
             <button>Login</button>
             <img src={cart_icon} alt="" />
+            <div className="nav-cart-count">0</div>
         </div>
     </div>
   )
