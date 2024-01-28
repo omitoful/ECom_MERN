47a3bf75927a08b6d3ede37167ce4669ae414c2d
Kevin <kevin@twjoin.com>
2024-01-28 15:40:03 +0800

    setup product page

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>
+            For your business, too, your customers’ wins are your wins. If you’re a small retailer, this is true not just with your in-store customer service, but also your ecommerce website’s overall UX, its check-out flow, your online return options, and your product descriptions. By writing accurate, educational, and engaging product descriptions, a copywriter serves their customers just as much as a sales associate does when someone walks through your shop’s front door.
+            </p>
+            <p>
+            For your business, too, your customers’ wins are your wins. If you’re a small retailer, this is true not just with your in-store customer service, but also your ecommerce website’s overall UX, its check-out flow, your online return options, and your product descriptions. By writing accurate, educational, and engaging product descriptions, a copywriter serves their customers just as much as a sales associate does when someone walks through your shop’s front door.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
