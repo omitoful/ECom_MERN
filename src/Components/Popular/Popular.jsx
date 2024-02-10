fbebe763e0abb4887e0cdb611d545d560f290f02
Kevin <kevin@twjoin.com>
2024-02-10 14:52:07 +0800

    fetch popular & new collection data

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Popular.css'
-import data_product from '../Assets/data'
 import Item from '../Item/Item'
 
 const Popular = () => {
+
+  const [popular_data, setPopular_Data] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/popularinwomen")
+    .then((res) => res.json())
+    .then((data) => setPopular_Data(data))
+  }, [])
+
   return (
     <div className='popular'>
         <h1>POPULAR IN WOMEN</h1>
         <hr/>
         <div className="popular-item">
-            {data_product.map((item, i) => {
+            {popular_data.map((item, i) => {
                 return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
             })}
         </div>
